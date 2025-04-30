830661e671f49710498ec4faecaf2f52aff73143
Victor Bruce <victorbruce82@gmail.com>
2025-04-30 14:12:45 +0000

    fix(overlay): assign the right overlay color to the selected room image

--- main.js
+++ main.js
@@ -142,13 +142,13 @@ const rooms = [
   },
 ];
 
-const warmOverlay = `linear-gradient(
+const coolOverlay = `linear-gradient(
     to bottom,
     rgba(141, 158, 247, 0.2),
     rgba(194, 197, 215, 0.1)
   )`;
 
-const coolOverlay = `linear-gradient(to bottom, rgba(236, 96, 98, 0.2), rgba(248, 210, 211, 0.13))`;
+const warmOverlay = `linear-gradient(to bottom, rgba(236, 96, 98, 0.2), rgba(248, 210, 211, 0.13))`;
 
 const setInitialOverlay = () => {
   document.querySelector(
