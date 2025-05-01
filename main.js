cc30c33224337efb22711644cbbd933c18e603f9
Victor Bruce <victorbruce82@gmail.com>
2025-05-01 11:01:11 +0000

    fix: display the right text message when the ac is powered on

--- main.js
+++ main.js
@@ -359,7 +359,7 @@ const generateRooms = () => {
          
           <span class="room-status" style="display: ${
             room.airConditionerOn ? "" : "none"
-          }">${room.currTemp > 25 ? "Cooling room to: " : "Warming room to: "}${
+          }">${room.currTemp < 25 ? "Cooling room to: " : "Warming room to: "}${
       room.currTemp
     }°</span>
         </div>
